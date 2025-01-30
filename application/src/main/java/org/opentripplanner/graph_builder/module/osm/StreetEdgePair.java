6d34e80d83aa19eeea39b48965befd2717e0343e
Michael Tsang <michael.tsang@jnction.co.uk>
2025-01-30 15:18:19 +0000

    revert changes

--- application/src/main/java/org/opentripplanner/graph_builder/module/osm/StreetEdgePair.java
+++ application/src/main/java/org/opentripplanner/graph_builder/module/osm/StreetEdgePair.java
@@ -1,10 +1,9 @@
 package org.opentripplanner.graph_builder.module.osm;
 
 import java.util.ArrayList;
-import javax.annotation.Nullable;
 import org.opentripplanner.street.model.edge.StreetEdge;
 
-public record StreetEdgePair(@Nullable StreetEdge main, @Nullable StreetEdge back) {
+public record StreetEdgePair(StreetEdge main, StreetEdge back) {
   /**
    * Return the non-null elements of this pair as an Iterable.
    */
