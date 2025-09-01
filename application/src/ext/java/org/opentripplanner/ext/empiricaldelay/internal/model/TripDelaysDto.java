238791277907bb627d805c8656bc4b88ada4aced
Thomas Gran <t2gran@gmail.com>
2025-09-02 00:05:16 +0200

    feat: Add DTOs for trip delays and delays at stop

---
+++ application/src/ext/java/org/opentripplanner/ext/empiricaldelay/internal/model/TripDelaysDto.java
@@ -0,0 +1,40 @@
+package org.opentripplanner.ext.empiricaldelay.internal.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.opentripplanner.ext.empiricaldelay.model.EmpiricalDelay;
+import org.opentripplanner.transit.model.framework.FeedScopedId;
+
+public class TripDelaysDto {
+
+  private final FeedScopedId tripId;
+  private final Map<String, List<DelayAtStopDto>> delaysForServiceId = new HashMap<>();
+
+  public TripDelaysDto(FeedScopedId tripId) {
+    this.tripId = tripId;
+  }
+
+  public FeedScopedId tripId() {
+    return tripId;
+  }
+
+  public void addDelay(
+    String calendarServiceId,
+    int sequence,
+    FeedScopedId stopId,
+    EmpiricalDelay empiricalDelay
+  ) {
+    var list = delaysForServiceId.computeIfAbsent(calendarServiceId, id -> new ArrayList<>());
+    list.add(new DelayAtStopDto(sequence, stopId, empiricalDelay));
+  }
+
+  public Iterable<String> serviceIds() {
+    return delaysForServiceId.keySet();
+  }
+
+  public List<DelayAtStopDto> delaysForServiceId(String serviceId) {
+    return delaysForServiceId.get(serviceId);
+  }
+}
