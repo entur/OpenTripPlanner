d92eb0a3d9c384f35cfb15fac8947c323a7298ef
Leonard Ehrenfried <mail@leonard.io>
2025-10-16 11:57:48 +0200

    Convert to paramterised test

--- application/src/ext-test/java/org/opentripplanner/ext/fares/impl/gtfs/FareLookupServiceTest.java
+++ application/src/ext-test/java/org/opentripplanner/ext/fares/impl/gtfs/FareLookupServiceTest.java
@@ -6,8 +6,15 @@ import static org.opentripplanner.transit.model._data.TimetableRepositoryForTest
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import java.util.Collection;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.opentripplanner.ext.fares.impl._support.FareTestConstants;
 import org.opentripplanner.ext.fares.model.FareLegRule;
 import org.opentripplanner.ext.fares.model.FareLegRuleBuilder;
@@ -26,73 +33,105 @@ class FareLookupServiceTest implements FareTestConstants {
     .build();
   private static final ImmutableMultimap<FeedScopedId, FeedScopedId> EMPTY_STOP_AREAS =
     ImmutableMultimap.of();
-  private final TimetableRepositoryForTest testModel = TimetableRepositoryForTest.of();
-  private final RegularStop STOP_1 = testModel.stop("stop1").build();
-  private final RegularStop STOP_2 = testModel.stop("stop2").build();
-  private final Multimap<FeedScopedId, FeedScopedId> stopAreas = ImmutableMultimap.of(
-    STOP_1.getId(),
+  private static final FeedScopedId STOP1_ID = id("stop1");
+  private static final FeedScopedId STOP2_ID = id("stop2");
+  private static final Multimap<FeedScopedId, FeedScopedId> STOP_AREAS = ImmutableMultimap.of(
+    STOP1_ID,
     A_1,
-    STOP_2.getId(),
+    STOP2_ID,
     A_1
   );
+  private static final FeedScopedId R_1 = TimetableRepositoryForTest.id("r1");
+  private static final FeedScopedId R_2 = TimetableRepositoryForTest.id("r2");
 
-  @Test
-  void mixedAreaAndNetworkWithoutPriorityLeadsToEmptyResults() {
-    var r1 = rule1().withNetworkId(NETWORK_A.getId()).build();
-    var r2 = rule2().withFromAreaId(A_1).withToAreaId(A_1).build();
-    var service = new FareLookupService(List.of(r1, r2), List.of(), stopAreas);
+  private final TimetableRepositoryForTest testModel = TimetableRepositoryForTest.of();
+  private final RegularStop STOP_1 = testModel.stop(STOP1_ID.getId()).build();
+  private final RegularStop STOP_2 = testModel.stop(STOP2_ID.getId()).build();
 
-    assertThat(service.legRules(leg())).isEmpty();
+  private record TestCase(
+    String name,
+    Function<FareLegRuleBuilder, FareLegRuleBuilder> rule1Customiser,
+    Function<FareLegRuleBuilder, FareLegRuleBuilder> rule2Customiser,
+    Multimap<FeedScopedId, FeedScopedId> stopAreas,
+    Collection<FeedScopedId> expectedResults
+  )
+    implements Named {
+    @Override
+    public String getName() {
+      return name;
+    }
+
+    @Override
+    public Object getPayload() {
+      return this;
+    }
   }
 
-  @Test
-  void priorityLeadsToResults() {
-    var r1 = rule1().withNetworkId(NETWORK_A.getId()).withPriority(0).build();
-    var r2 = rule2().withFromAreaId(A_1).withToAreaId(A_1).withPriority(2).build();
-    var service = new FareLookupService(List.of(r1, r2), List.of(), stopAreas);
-
-    assertThat(service.legRules(leg())).containsExactly(r2);
+  private static Stream<Arguments> cases() {
+    return Stream.of(
+      new TestCase(
+        "mixed area and network without priority leads to zero results",
+        r1 -> r1.withNetworkId(NETWORK_A.getId()),
+        r2 -> r2.withFromAreaId(A_1).withToAreaId(A_1),
+        STOP_AREAS,
+        Set.of()
+      ),
+      new TestCase(
+        "adding a priority leads to some results",
+        r1 -> r1.withNetworkId(NETWORK_A.getId()).withPriority(0),
+        r2 -> r2.withFromAreaId(A_1).withToAreaId(A_1).withPriority(2),
+        STOP_AREAS,
+        Set.of(R_2)
+      ),
+      new TestCase(
+        "network wildcard",
+        r1 -> r1.withFromAreaId(A_1).withToAreaId(A_1),
+        r2 -> r2.withFromAreaId(A_1).withToAreaId(A_1),
+        STOP_AREAS,
+        Set.of(R_1, R_2)
+      ),
+      new TestCase(
+        "conflicting results resolved by priority",
+        r1 -> r1.withNetworkId(NETWORK_A.getId()).withPriority(1),
+        r2 -> r2.withNetworkId(NETWORK_A.getId()).withPriority(2),
+        STOP_AREAS,
+        Set.of(R_2)
+      ),
+      new TestCase(
+        "no priority returns both",
+        r1 -> r1.withNetworkId(NETWORK_A.getId()),
+        r2 -> r2.withNetworkId(NETWORK_A.getId()),
+        EMPTY_STOP_AREAS,
+        Set.of(R_1, R_2)
+      ),
+      new TestCase(
+        "multiple rules with equal priority",
+        r1 -> r1.withNetworkId(NETWORK_A.getId()).withPriority(10),
+        r2 -> r2.withNetworkId(NETWORK_A.getId()).withPriority(10),
+        EMPTY_STOP_AREAS,
+        Set.of(R_1, R_2)
+      )
+    ).map(Arguments::of);
   }
 
-  @Test
-  void networkWildCard() {
-    var r1 = rule1().withFromAreaId(A_1).withToAreaId(A_1).build();
-    var r2 = rule2().withFromAreaId(A_1).withToAreaId(A_1).build();
-    var service = new FareLookupService(List.of(r1, r2), List.of(), stopAreas);
+  @ParameterizedTest
+  @MethodSource("cases")
+  void twoRules(TestCase tc) {
+    var r1 = tc.rule1Customiser.apply(rule1()).build();
+    var r2 = tc.rule2Customiser.apply(rule2()).build();
+    var service = new FareLookupService(List.of(r1, r2), List.of(), STOP_AREAS);
 
-    assertThat(service.legRules(leg())).containsExactly(r1, r2);
-  }
-
-  @Test
-  void conflictingRulesResolvedByPriority() {
-    var r1 = rule1().withNetworkId(NETWORK_A.getId()).withPriority(1).build();
-    var r2 = rule2().withNetworkId(NETWORK_A.getId()).withPriority(2).build();
-    var service = new FareLookupService(List.of(r1, r2), List.of(), EMPTY_STOP_AREAS);
-    assertThat(service.legRules(leg())).containsExactly(r2);
-  }
-
-  @Test
-  void noPriorityReturnsBoth() {
-    var r1 = rule1().withNetworkId(NETWORK_A.getId()).build();
-    var r2 = rule2().withNetworkId(NETWORK_A.getId()).build();
-    var service = new FareLookupService(List.of(r1, r2), List.of(), EMPTY_STOP_AREAS);
-    assertThat(service.legRules(leg())).containsExactly(r1, r2);
-  }
-
-  @Test
-  void multipleRulesWithEqualPriority() {
-    var r1 = rule1().withNetworkId(NETWORK_A.getId()).withPriority(10).build();
-    var r2 = rule2().withNetworkId(NETWORK_A.getId()).withPriority(10).build();
-    var service = new FareLookupService(List.of(r1, r2), List.of(), EMPTY_STOP_AREAS);
-    assertThat(service.legRules(leg())).containsExactly(r1, r2);
+    assertThat(service.legRules(leg()).stream().map(FareLegRule::id)).containsExactlyElementsIn(
+      tc.expectedResults
+    );
   }
 
   private static FareLegRuleBuilder rule1() {
-    return FareLegRule.of(id("r1"), List.of(FARE_PRODUCT_A));
+    return FareLegRule.of(R_1, List.of(FARE_PRODUCT_A));
   }
 
   private static FareLegRuleBuilder rule2() {
-    return FareLegRule.of(id("r2"), List.of(FARE_PRODUCT_B));
+    return FareLegRule.of(R_2, List.of(FARE_PRODUCT_B));
   }
 
   private TransitLeg leg() {
