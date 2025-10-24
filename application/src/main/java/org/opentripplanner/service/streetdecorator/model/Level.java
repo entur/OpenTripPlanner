2796b20ef75f2fb49b307dad3d84710cd51d1051
Ville Pihlava <ville.pihlava@cgi.com>
2025-10-24 11:36:16 +0300

    Add documentation for Level.

--- application/src/main/java/org/opentripplanner/service/streetdecorator/model/Level.java
+++ application/src/main/java/org/opentripplanner/service/streetdecorator/model/Level.java
@@ -1,3 +1,6 @@
 package org.opentripplanner.service.streetdecorator.model;
 
+/**
+ * Represents a level with a comparable 0-based number and name.
+ */
 public record Level(double level, String name) {}
