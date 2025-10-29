9db9e2ccaadb498a3800c7698fbebbbb920ae74a
Thomas Gran <t2gran@gmail.com>
2025-10-29 15:28:25 +0100

    review: Reduce the request scope, by only exposing the locale

--- application/src/main/java/org/opentripplanner/apis/transmodel/TransmodelGraphQLPlanner.java
+++ application/src/main/java/org/opentripplanner/apis/transmodel/TransmodelGraphQLPlanner.java
@@ -10,9 +10,7 @@ import org.opentripplanner.apis.transmodel.mapping.TripRequestMapper;
 import org.opentripplanner.apis.transmodel.mapping.ViaRequestMapper;
 import org.opentripplanner.apis.transmodel.model.PlanResponse;
 import org.opentripplanner.routing.algorithm.mapping.TripPlanMapper;
-import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.RouteRequestBuilder;
-import org.opentripplanner.routing.api.request.RouteViaRequest;
 import org.opentripplanner.routing.api.response.RoutingResponse;
 import org.opentripplanner.routing.api.response.ViaRoutingResponse;
 import org.opentripplanner.routing.error.RoutingValidationException;
@@ -33,11 +31,11 @@ public class TransmodelGraphQLPlanner {
 
   public DataFetcherResult<PlanResponse> plan(DataFetchingEnvironment environment) {
     TransmodelRequestContext ctx = environment.getContext();
-    RouteRequest request = null;
+    Locale locale;
     PlanResponse response;
     RouteRequestBuilder requestBuilder = tripRequestMapper.createRequestBuilder(environment);
     try {
-      request = requestBuilder.buildRequest();
+      var request = requestBuilder.buildRequest();
       RoutingResponse res = ctx.getRoutingService().route(request);
       response = PlanResponse.of()
         .withPlan(res.getTripPlan())
@@ -47,14 +45,14 @@ public class TransmodelGraphQLPlanner {
         .withPreviousPageCursor(res.getPreviousPageCursor())
         .withNextPageCursor(res.getNextPageCursor())
         .build();
+      locale = request.preferences().locale();
     } catch (RoutingValidationException e) {
       response = PlanResponse.of()
         .withPlan(TripPlanMapper.mapEmptyTripPlan(requestBuilder))
         .withMessages(e.getRoutingErrors())
         .build();
+      locale = defaultLocale(ctx);
     }
-    // The request can be null if the request builder encounters a RoutingValidationException.
-    Locale locale = request == null ? defaultLocale(ctx) : request.preferences().locale();
     return DataFetcherResult.<PlanResponse>newResult()
       .data(response)
       .localContext(Map.of("locale", locale))
@@ -64,16 +62,15 @@ public class TransmodelGraphQLPlanner {
   public DataFetcherResult<ViaRoutingResponse> planVia(DataFetchingEnvironment environment) {
     ViaRoutingResponse response;
     TransmodelRequestContext ctx = environment.getContext();
-    RouteViaRequest request = null;
+    Locale locale;
     try {
-      request = viaRequestMapper.createRouteViaRequest(environment);
+      var request = viaRequestMapper.createRouteViaRequest(environment);
       response = ctx.getRoutingService().route(request);
+      locale = request.locale();
     } catch (RoutingValidationException e) {
       response = new ViaRoutingResponse(Map.of(), List.of(), e.getRoutingErrors());
+      locale = defaultLocale(ctx);
     }
-
-    // The request can be null if the request mapper encounters a RoutingValidationException.
-    Locale locale = request == null ? defaultLocale(ctx) : request.locale();
     return DataFetcherResult.<ViaRoutingResponse>newResult()
       .data(response)
       .localContext(Map.of("locale", locale))
