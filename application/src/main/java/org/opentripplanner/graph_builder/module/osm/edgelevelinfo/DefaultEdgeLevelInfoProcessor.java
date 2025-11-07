0da7def9f7502ec57ea99c084f248af2bfc19fd0
Ville Pihlava <ville.pihlava@cgi.com>
2025-11-07 12:09:07 +0200

    Add ContradictoryLevelAndInclineInfoForWay issue.

--- application/src/main/java/org/opentripplanner/graph_builder/module/osm/edgelevelinfo/DefaultEdgeLevelInfoProcessor.java
+++ application/src/main/java/org/opentripplanner/graph_builder/module/osm/edgelevelinfo/DefaultEdgeLevelInfoProcessor.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.annotation.Nullable;
 import org.opentripplanner.graph_builder.issue.api.DataImportIssueStore;
+import org.opentripplanner.graph_builder.issues.ContradictoryLevelAndInclineInfoForWay;
 import org.opentripplanner.graph_builder.issues.CouldNotApplyMultiLevelInfoToWay;
 import org.opentripplanner.graph_builder.module.osm.OsmDatabase;
 import org.opentripplanner.osm.model.OsmLevel;
@@ -29,58 +30,85 @@ public class DefaultEdgeLevelInfoProcessor implements EdgeLevelInfoProcessor {
 
   @Override
   public Optional<EdgeLevelInfo> findEdgeLevelInfo(OsmDatabase osmdb, OsmWay way) {
-    List<OsmLevel> levels = osmdb.getLevelsForEntity(way);
     var nodeRefs = way.getNodeRefs();
     long firstNodeRef = nodeRefs.get(0);
     long lastNodeRef = nodeRefs.get(nodeRefs.size() - 1);
+
+    EdgeLevelInfo levelInfo = findLevelInfo(osmdb, way, firstNodeRef, lastNodeRef);
+    EdgeLevelInfo inclineInfo = findInclineInfo(osmdb, way, firstNodeRef, lastNodeRef);
+
+    if (
+      levelInfo != null &&
+      inclineInfo != null &&
+      levelInfo.lowerVertexInfo() != inclineInfo.lowerVertexInfo()
+    ) {
+      issueStore.add(new ContradictoryLevelAndInclineInfoForWay(way));
+      // Default to level info in case of contradictory information. Ideally this should be from
+      // the tag that is more reliable.
+      return Optional.of(levelInfo);
+    } else if (levelInfo != null) {
+      return Optional.of(levelInfo);
+    } else if (inclineInfo != null) {
+      return Optional.of(inclineInfo);
+    }
+    return Optional.empty();
+  }
+
+  private EdgeLevelInfo findLevelInfo(
+    OsmDatabase osmdb,
+    OsmWay way,
+    long firstNodeRef,
+    long lastNodeRef
+  ) {
+    List<OsmLevel> levels = osmdb.getLevelsForEntity(way);
     if (levels.size() == 2) {
       OsmLevel firstVertexOsmLevel = levels.get(0);
       OsmLevel lastVertexOsmLevel = levels.get(1);
       if (firstVertexOsmLevel.level() < lastVertexOsmLevel.level()) {
-        return Optional.of(
-          new EdgeLevelInfo(
-            new VertexLevelInfo(
-              new Level(firstVertexOsmLevel.level(), firstVertexOsmLevel.name()),
-              firstNodeRef
-            ),
-            new VertexLevelInfo(
-              new Level(lastVertexOsmLevel.level(), lastVertexOsmLevel.name()),
-              lastNodeRef
-            )
+        return new EdgeLevelInfo(
+          new VertexLevelInfo(
+            new Level(firstVertexOsmLevel.level(), firstVertexOsmLevel.name()),
+            firstNodeRef
+          ),
+          new VertexLevelInfo(
+            new Level(lastVertexOsmLevel.level(), lastVertexOsmLevel.name()),
+            lastNodeRef
           )
         );
       } else if (firstVertexOsmLevel.level() > lastVertexOsmLevel.level()) {
-        return Optional.of(
-          new EdgeLevelInfo(
-            new VertexLevelInfo(
-              new Level(lastVertexOsmLevel.level(), lastVertexOsmLevel.name()),
-              lastNodeRef
-            ),
-            new VertexLevelInfo(
-              new Level(firstVertexOsmLevel.level(), firstVertexOsmLevel.name()),
-              firstNodeRef
-            )
+        return new EdgeLevelInfo(
+          new VertexLevelInfo(
+            new Level(lastVertexOsmLevel.level(), lastVertexOsmLevel.name()),
+            lastNodeRef
+          ),
+          new VertexLevelInfo(
+            new Level(firstVertexOsmLevel.level(), firstVertexOsmLevel.name()),
+            firstNodeRef
           )
         );
       }
     }
+    return null;
+  }
 
+  private EdgeLevelInfo findInclineInfo(
+    OsmDatabase osmdb,
+    OsmWay way,
+    long firstNodeRef,
+    long lastNodeRef
+  ) {
     if (way.isInclineUp()) {
-      return Optional.of(
-        new EdgeLevelInfo(
-          new VertexLevelInfo(null, firstNodeRef),
-          new VertexLevelInfo(null, lastNodeRef)
-        )
+      return new EdgeLevelInfo(
+        new VertexLevelInfo(null, firstNodeRef),
+        new VertexLevelInfo(null, lastNodeRef)
       );
     } else if (way.isInclineDown()) {
-      return Optional.of(
-        new EdgeLevelInfo(
-          new VertexLevelInfo(null, lastNodeRef),
-          new VertexLevelInfo(null, firstNodeRef)
-        )
+      return new EdgeLevelInfo(
+        new VertexLevelInfo(null, lastNodeRef),
+        new VertexLevelInfo(null, firstNodeRef)
       );
     }
-    return Optional.empty();
+    return null;
   }
 
   @Override
