4a4c7853bfec26e95d8b0a02058af48a79410306
Thomas Gran <t2gran@gmail.com>
2025-11-12 19:12:30 +0100

    refactor: Cleanup MinMap and add unit test

--- application/src/main/java/org/opentripplanner/graph_builder/module/transfer/filter/MinMap.java
+++ application/src/main/java/org/opentripplanner/graph_builder/module/transfer/filter/MinMap.java
@@ -1,13 +1,17 @@
 package org.opentripplanner.graph_builder.module.transfer.filter;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A HashMap that has been extended to track the greatest or smallest value for each key. Note that
  * this does not change the meaning of the 'put' method. It adds two new methods that add the
  * min/max behavior. This class used to be inside SimpleIsochrone.
  */
-class MinMap<K, V extends Comparable<V>> extends HashMap<K, V> {
+class MinMap<K, V extends Comparable<V>> {
+
+  private final Map<K, V> map = new HashMap<>();
 
   /**
    * Put the given key-value pair in the map if the map does not yet contain the key, or if the
@@ -16,26 +20,25 @@ class MinMap<K, V extends Comparable<V>> extends HashMap<K, V> {
    * @return whether the key-value pair was inserted in the map.
    */
   boolean putMin(K key, V value) {
-    V oldValue = this.get(key);
+    V oldValue = map.get(key);
     if (oldValue == null || value.compareTo(oldValue) < 0) {
-      this.put(key, value);
+      map.put(key, value);
       return true;
     }
     return false;
   }
 
   /**
-   * Put the given key-value pair in the map if the map does not yet contain the key, or if the
-   * value is greater than the existing value for the same key.
-   *
-   * @return whether the key-value pair was inserted in the map.
+   * @see Map#get(Object)
    */
-  boolean putMax(K key, V value) {
-    V oldValue = this.get(key);
-    if (oldValue == null || value.compareTo(oldValue) > 0) {
-      this.put(key, value);
-      return true;
-    }
-    return false;
+  public V get(K key) {
+    return map.get(key);
+  }
+
+  /**
+   * @see Map#values()
+   */
+  public Collection<V> values() {
+    return map.values();
   }
 }
