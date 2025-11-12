9e57fc8fbc35663085731b8629d91f6c36a512ce
Leonard Ehrenfried <mail@leonard.io>
2025-11-12 10:36:30 +0100

    Add better serialization test

--- application/src/test/java/org/opentripplanner/model/fare/SerializationTest.java
+++ application/src/test/java/org/opentripplanner/model/fare/SerializationTest.java
@@ -1,11 +1,11 @@
 package org.opentripplanner.model.fare;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,33 +13,25 @@ import org.opentripplanner.ext.fares.model.FareTransferRule;
 
 class SerializationTest {
 
+  public static final List<Class<?>> CLASSES = List.of(
+    FareProduct.class,
+    FareTransferRule.class,
+    FareMedium.class,
+    RiderCategory.class
+  );
+
   static Stream<Class<?>> cases() {
-    return Stream.of(
-      FareProduct.class,
-      FareTransferRule.class,
-      FareMedium.class,
-      RiderCategory.class
-    );
+    var fields = CLASSES.stream()
+      .flatMap(c ->
+        Arrays.stream(c.getDeclaredFields()).map(Field::getType).filter(type -> !type.isPrimitive())
+      );
+
+    return Stream.concat(CLASSES.stream(), fields).distinct();
   }
 
   @ParameterizedTest
   @MethodSource("cases")
-  void testFareProductSerializable(Class<?> clazz) {
+  void serializable(Class<?> clazz) {
     assertInstanceOf(Serializable.class, clazz);
-
-    for (Field field : clazz.getDeclaredFields()) {
-      field.setAccessible(true);
-      Class<?> fieldType = field.getType();
-      if (
-        !fieldType.isPrimitive() &&
-        !fieldType.isRecord() &&
-        !fieldType.isAssignableFrom(Collection.class)
-      ) {
-        assertTrue(
-          Serializable.class.isAssignableFrom(fieldType),
-          "Field " + field.getName() + " must be serializable"
-        );
-      }
-    }
   }
 }
