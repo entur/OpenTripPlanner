6b51f70c8dd5c179447dbdc22a5c1c1f7da11a1d
Leonard Ehrenfried <mail@leonard.io>
2025-12-26 10:47:37 +0100

    Introduce HTTP handler for TRIAS/OJP

--- application/src/ext/java/org/opentripplanner/ext/ojp/trias/TriasResource.java
+++ application/src/ext/java/org/opentripplanner/ext/ojp/trias/TriasResource.java
@@ -1,8 +1,6 @@
 package org.opentripplanner.ext.ojp.trias;
 
 import de.vdv.ojp20.OJP;
-import de.vdv.ojp20.OJPStopEventRequestStructure;
-import de.vdv.ojp20.siri.AbstractFunctionalServiceRequestStructure;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -16,21 +14,16 @@ import jakarta.xml.bind.JAXBException;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.time.ZonedDateTime;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import javax.xml.transform.TransformerException;
 import org.opentripplanner.api.model.transit.DefaultFeedIdMapper;
 import org.opentripplanner.api.model.transit.FeedScopedIdMapper;
 import org.opentripplanner.api.model.transit.HideFeedIdMapper;
-import org.opentripplanner.ext.ojp.mapping.ErrorMapper;
 import org.opentripplanner.ext.ojp.parameters.TriasApiParameters;
 import org.opentripplanner.ext.ojp.service.OjpService;
 import org.opentripplanner.ext.ojp.service.OjpServiceMapper;
-import org.opentripplanner.routing.error.RoutingValidationException;
 import org.opentripplanner.standalone.api.OtpServerRequestContext;
-import org.opentripplanner.transit.model.framework.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,23 +36,15 @@ public class TriasResource {
     "stop-event-stop-point.xml"
   );
 
-  private final OjpServiceMapper ojpService;
+  private final OjpRequestHandler handler;
 
   public TriasResource(@Context OtpServerRequestContext context) {
     var transitService = context.transitService();
     var zoneId = context.triasApiParameters().timeZone().orElse(transitService.getTimeZone());
-    var vdvService = new OjpService(context.transitService(), context.graphFinder());
-
-    FeedScopedIdMapper idMapper = idMapper(context.triasApiParameters());
-    this.ojpService = new OjpServiceMapper(vdvService, idMapper, zoneId);
-  }
-
-  private FeedScopedIdMapper idMapper(TriasApiParameters triasApiConfig) {
-    if (triasApiConfig.hideFeedId()) {
-      return new HideFeedIdMapper(triasApiConfig.hardcodedInputFeedId());
-    } else {
-      return new DefaultFeedIdMapper();
-    }
+    var service = new OjpService(transitService, context.graphFinder());
+    var idMapper = idMapper(context.triasApiParameters());
+    var serviceMapper = new OjpServiceMapper(service, idMapper, zoneId);
+    this.handler = new OjpRequestHandler(serviceMapper, TriasResource::ojpToTrias, "TRIAS");
   }
 
   @POST
@@ -67,50 +52,11 @@ public class TriasResource {
   public Response index(String triasInput) {
     try {
       var ojp = OjpToTriasTransformer.triasToOjp(triasInput);
-
-      var request = findRequest(ojp);
-
-      if (request instanceof OJPStopEventRequestStructure ser) {
-        var ojpResponse = ojpService.handleStopEventRequest(ser);
-        final StreamingOutput stream = ojpToTrias(ojpResponse);
-        return Response.ok(stream).build();
-      } else {
-        return error(
-          "Request type '%s' is not supported".formatted(request.getClass().getSimpleName())
-        );
-      }
-    } catch (EntityNotFoundException | RoutingValidationException e) {
-      return error(e.getMessage());
+      return handler.handleRequest(ojp);
     } catch (JAXBException | TransformerException e) {
       LOG.error("Error reading TRIAS request", e);
-      return error("Could not read TRIAS request.");
-    } catch (Exception e) {
-      LOG.error("Error processing TRIAS request", e);
-      return error(e.getMessage());
+      return handler.error("Could not read TRIAS request.");
     }
-  }
-
-  private static AbstractFunctionalServiceRequestStructure findRequest(OJP ojp) {
-    return Optional.ofNullable(ojp.getOJPRequest())
-      .map(s -> s.getServiceRequest())
-      .stream()
-      .flatMap(s -> s.getAbstractFunctionalServiceRequest().stream())
-      .findFirst()
-      .orElseThrow(() -> new IllegalArgumentException("No request found in TRIAS XML body."))
-      .getValue();
-  }
-
-  private static Response error(String value) {
-    var trias = ojpToTrias(ErrorMapper.error(value, ZonedDateTime.now()));
-    return Response.status(Response.Status.BAD_REQUEST).entity(trias).build();
-  }
-
-  private static StreamingOutput ojpToTrias(OJP ojpOutput) {
-    return os -> {
-      Writer writer = new OutputStreamWriter(os);
-      OjpToTriasTransformer.ojpToTrias(ojpOutput, writer);
-      writer.flush();
-    };
   }
 
   @GET
@@ -143,6 +89,22 @@ public class TriasResource {
     } else {
       return Response.status(Response.Status.NOT_FOUND).build();
     }
+  }
+
+  private static FeedScopedIdMapper idMapper(TriasApiParameters triasApiConfig) {
+    if (triasApiConfig.hideFeedId()) {
+      return new HideFeedIdMapper(triasApiConfig.hardcodedInputFeedId());
+    } else {
+      return new DefaultFeedIdMapper();
+    }
+  }
+
+  private static StreamingOutput ojpToTrias(OJP ojpOutput) {
+    return os -> {
+      Writer writer = new OutputStreamWriter(os);
+      OjpToTriasTransformer.ojpToTrias(ojpOutput, writer);
+      writer.flush();
+    };
   }
 
   private static Response classpathResource(String name) throws IOException {
