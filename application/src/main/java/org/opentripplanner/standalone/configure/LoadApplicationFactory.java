e2ae97efb8861fff51985304fb3838aa46747b43
Leonard Ehrenfried <mail@leonard.io>
2026-01-07 12:31:10 +0100

    Fix formatting

--- application/src/main/java/org/opentripplanner/standalone/configure/LoadApplicationFactory.java
+++ application/src/main/java/org/opentripplanner/standalone/configure/LoadApplicationFactory.java
@@ -51,7 +51,7 @@ import org.opentripplanner.transit.service.TimetableRepository;
     StopConsolidationRepositoryModule.class,
     StreetRepositoryModule.class,
     VehicleParkingRepositoryModule.class,
-    FareModule.class
+    FareModule.class,
   }
 )
 public interface LoadApplicationFactory {
