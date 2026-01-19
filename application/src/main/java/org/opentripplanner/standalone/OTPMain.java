a934389c6b8f5432684147072af688bd94f08e7a
Leonard Ehrenfried <mail@leonard.io>
2026-01-19 11:20:36 +0100

    Index during webserver start

--- application/src/main/java/org/opentripplanner/standalone/OTPMain.java
+++ application/src/main/java/org/opentripplanner/standalone/OTPMain.java
@@ -187,6 +187,7 @@ public class OTPMain {
   private static void startOtpWebServer(CommandLineParameters params, ConstructApplication app) {
     // Index graph for travel search
     app.timetableRepository().index();
+    app.transferRepository().index();
     app.graph().index();
 
     // publishing the config version info make it available to the APIs
--- application/src/test/java/org/opentripplanner/transit/speed_test/SetupHelper.java
+++ application/src/test/java/org/opentripplanner/transit/speed_test/SetupHelper.java
@@ -33,6 +33,7 @@ class SetupHelper {
     TimetableRepository timetableRepository = serializedGraphObject.timetableRepository;
     TransferRepository transferRepository = serializedGraphObject.transferRepository;
     timetableRepository.index();
+    transferRepository.index();
     graph.index();
     return new LoadModel(
       graph,
