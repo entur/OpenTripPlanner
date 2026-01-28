5f0dadbc73ba0b1411a2348d99e1a526aa72e81c
Vincent Paturet <vincent.paturet@entur.org>
2026-01-28 15:09:39 +0100

    Fix deletion of changed pattern

--- application/src/main/java/org/opentripplanner/updater/trip/handlers/UpdateExistingTripHandler.java
+++ application/src/main/java/org/opentripplanner/updater/trip/handlers/UpdateExistingTripHandler.java
@@ -17,6 +17,7 @@ import org.opentripplanner.transit.service.TransitEditorService;
 import org.opentripplanner.updater.spi.DataValidationExceptionMapper;
 import org.opentripplanner.updater.spi.UpdateError;
 import org.opentripplanner.updater.trip.StopResolver;
+import org.opentripplanner.updater.trip.TimetableSnapshotManager;
 import org.opentripplanner.updater.trip.TripUpdateApplierContext;
 import org.opentripplanner.updater.trip.gtfs.BackwardsDelayInterpolator;
 import org.opentripplanner.updater.trip.gtfs.ForwardsDelayInterpolator;
@@ -112,6 +113,10 @@ public class UpdateExistingTripHandler implements TripUpdateHandler {
         // SiriTripPatternCache uses 2-parameter signature (gets original pattern via injected function)
         finalPattern = tripPatternCache.getOrCreateTripPattern(newStopPattern, trip);
         realTimeState = RealTimeState.MODIFIED;
+
+        // Cancel the trip in the scheduled pattern since it's moving to a modified pattern
+        // This prevents the trip from appearing in both patterns in the routing data
+        markScheduledTripAsDeleted(trip, pattern, serviceDate, snapshotManager);
       }
     }
 
@@ -446,5 +451,41 @@ public class UpdateExistingTripHandler implements TripUpdateHandler {
     }
 
     return null;
+  }
+
+  /**
+   * Mark the scheduled trip in the buffer as deleted when moving to a modified pattern.
+   * This prevents the trip from appearing in both the scheduled and modified patterns.
+   *
+   * @param trip The trip to delete from the scheduled pattern
+   * @param scheduledPattern The scheduled pattern containing the trip
+   * @param serviceDate The service date
+   * @param snapshotManager The snapshot manager to update
+   */
+  private static void markScheduledTripAsDeleted(
+    Trip trip,
+    TripPattern scheduledPattern,
+    java.time.LocalDate serviceDate,
+    TimetableSnapshotManager snapshotManager
+  ) {
+    // Get the scheduled trip times from the scheduled timetable
+    final var scheduledTimetable = scheduledPattern.getScheduledTimetable();
+    final var scheduledTripTimes = scheduledTimetable.getTripTimes(trip);
+
+    if (scheduledTripTimes == null) {
+      LOG.warn("Could not mark scheduled trip as deleted: {}", trip.getId());
+      return;
+    }
+
+    // Create a deleted version of the trip times
+    final var builder = scheduledTripTimes.createRealTimeFromScheduledTimes();
+    builder.deleteTrip();
+
+    // Update the buffer with the deleted trip times in the scheduled pattern
+    snapshotManager.updateBuffer(
+      new RealTimeTripUpdate(scheduledPattern, builder.build(), serviceDate)
+    );
+
+    LOG.debug("Marked scheduled trip {} as deleted on {}", trip.getId(), serviceDate);
   }
 }
