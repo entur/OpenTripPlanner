4b32ebd0fe80fc255652d58e7b062926f6edd786
Sigurd Totland <sigurdtotland@gmail.com>
2026-01-30 14:57:03 +0100

    refactor: Move PropulsionType enum into street.model package

---
+++ application/src/main/java/org/opentripplanner/street/model/PropulsionType.java
@@ -0,0 +1,12 @@
+package org.opentripplanner.street.model;
+
+public enum PropulsionType {
+  HUMAN,
+  ELECTRIC_ASSIST,
+  ELECTRIC,
+  COMBUSTION,
+  COMBUSTION_DIESEL,
+  HYBRID,
+  PLUG_IN_HYBRID,
+  HYDROGEN_FUEL_CELL,
+}
