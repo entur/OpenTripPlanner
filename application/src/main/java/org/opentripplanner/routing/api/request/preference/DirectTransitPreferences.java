46fe8ee31baf716853e33807526ba6ad9480bbbf
Henrik Abrahamsson <henrik.abrahamsson@decerno.se>
2026-01-30 11:10:08 +0100

    Don't log the enabled flag when default

--- application/src/main/java/org/opentripplanner/routing/api/request/preference/DirectTransitPreferences.java
+++ application/src/main/java/org/opentripplanner/routing/api/request/preference/DirectTransitPreferences.java
@@ -107,7 +107,7 @@ public class DirectTransitPreferences {
   @Override
   public String toString() {
     return ToStringBuilder.of(DirectTransitPreferences.class)
-      .addBool("enabled", enabled)
+      .addBool("enabled", enabled, DEFAULT.enabled)
       .addObj("costRelaxFunction", costRelaxFunction, DEFAULT.costRelaxFunction)
       .addNum(
         "extraAccessEgressReluctance",
