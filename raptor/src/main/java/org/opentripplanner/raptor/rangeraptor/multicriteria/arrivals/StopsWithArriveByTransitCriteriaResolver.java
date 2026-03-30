17aed42634d8c863d73e8b50cb9250eef4ae7e0b
Thomas Gran <t2gran@gmail.com>
2026-03-30 15:03:54 +0200

    Update raptor/src/main/java/org/opentripplanner/raptor/rangeraptor/multicriteria/arrivals/StopsWithArriveByTransitCriteriaResolver.java
    Co-authored-by: Teemu Kalvas <teemu.kalvas@s2.org>

--- raptor/src/main/java/org/opentripplanner/raptor/rangeraptor/multicriteria/arrivals/StopsWithArriveByTransitCriteriaResolver.java
+++ raptor/src/main/java/org/opentripplanner/raptor/rangeraptor/multicriteria/arrivals/StopsWithArriveByTransitCriteriaResolver.java
@@ -11,8 +11,8 @@ import org.opentripplanner.raptor.rangeraptor.transit.ViaConnections;
 /**
  * Resolves the set of stops that require the "arrive-by-transit" Pareto criterion in
  * multi-criteria Raptor. At these stops, an arrival by transit (on-board) must be tracked
- * so that thise are not dominated by on-street arrivals(transfers & access). This is because
- * at these stops arriving on-board will give you an advantige - e.g. you must arrive by transit
+ * so that these are not dominated by on-street arrivals(transfers & access). This is because
+ * at these stops arriving on-board will give you an advantage - e.g. you must arrive by transit
  * to continue using a via-connection, transfer or egress.
  *
  * <p>A stop is included in the result when at least one of the following holds:
@@ -22,14 +22,14 @@ import org.opentripplanner.raptor.rangeraptor.transit.ViaConnections;
  *       The access should not be dominated by a transfer arrival - since using the
  *       access+transfer might be optimal.
  *       </li>
- *   <li><b>Egress witch start on-street(on foot):</b> an egress path leaving the stop on foot
+ *   <li><b>Egress which starts on-street(on foot):</b> an egress path leaving the stop on foot
  *       can only be used if the stop is reached by transit.</li>
  *   <li><b>Via transfer:</b> a via-connection departs from the stop as a transfer. Arriving
  *       at the stop by transit is the only way to use the via-connection; Hence it has an
- *       advantige over arriving by transfer.</li>
+ *       advantage over arriving by transfer.</li>
  * </ol>
  *
- * For all other stops the Raptor algoritmn with rounds and iteratins take care of this.
+ * For all other stops the Raptor algorithm with rounds and iterations take care of this.
  *
  */
 public class StopsWithArriveByTransitCriteriaResolver {
