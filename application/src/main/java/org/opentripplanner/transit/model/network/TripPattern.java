c198004cecafe45c15f8c32b13eb315dc6bd6df1
Leonard Ehrenfried <mail@leonard.io>
2026-04-01 17:40:43 +0200

    Remove stray import [ci skip]

--- application/src/main/java/org/opentripplanner/transit/model/network/TripPattern.java
+++ application/src/main/java/org/opentripplanner/transit/model/network/TripPattern.java
@@ -3,7 +3,6 @@ package org.opentripplanner.transit.model.network;
 import static java.util.Objects.requireNonNull;
 import static org.opentripplanner.utils.lang.ObjectUtils.requireNotInitialized;
 
-import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
