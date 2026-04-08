578e6867858044f70c5a5ae9f78c65a991da7b2a
Vincent Paturet <vincent.paturet@entur.org>
2026-04-08 08:37:55 +0200

    Add dedicated error message for no direct mode connection

--- application/src/main/java/org/opentripplanner/apis/support/mapping/PlannerErrorMapper.java
+++ application/src/main/java/org/opentripplanner/apis/support/mapping/PlannerErrorMapper.java
@@ -18,8 +18,10 @@ public class PlannerErrorMapper {
     switch (domain.code) {
       case NO_TRANSIT_CONNECTION:
       case NO_TRANSIT_CONNECTION_IN_SEARCH_WINDOW:
-      case NO_DIRECT_MODE_CONNECTION:
         api = new PlannerError(Message.PATH_NOT_FOUND);
+        break;
+      case NO_DIRECT_MODE_CONNECTION:
+        api = new PlannerError(Message.NO_DIRECT_MODE_CONNECTION);
         break;
       case OUTSIDE_BOUNDS:
         api = new PlannerError(Message.OUTSIDE_BOUNDS);
