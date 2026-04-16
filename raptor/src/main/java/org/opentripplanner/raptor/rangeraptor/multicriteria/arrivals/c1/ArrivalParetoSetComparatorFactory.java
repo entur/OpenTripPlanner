a7eebe0fbf9dcf19f89c4d81249f63136e1eaceb
Thomas Gran <t2gran@gmail.com>
2026-04-17 00:18:09 +0200

    refactor: Enhance ArrivalParetoSetComparatorFactory for improved performance and clarity

--- raptor/src/main/java/org/opentripplanner/raptor/rangeraptor/multicriteria/arrivals/c1/ArrivalParetoSetComparatorFactory.java
+++ raptor/src/main/java/org/opentripplanner/raptor/rangeraptor/multicriteria/arrivals/c1/ArrivalParetoSetComparatorFactory.java
@@ -5,119 +5,81 @@ import org.opentripplanner.raptor.api.model.DominanceFunction;
 import org.opentripplanner.raptor.api.model.RelaxFunction;
 import org.opentripplanner.raptor.util.paretoset.ParetoComparator;
 
-public interface ArrivalParetoSetComparatorFactory<T extends McStopArrival<?>> {
+/**
+ * This class creates and hold two {@code ParetoComparator}s witch is used in a single McRaptor
+ * search.
+ *
+ * The creation of the two {@link ParetoComparator}s should be done in such way that the JIT
+ * compiler can inline all lamdas for the best possible performance. Changes this this class
+ * should be checked with the SpeedTest to avoid degration in performance.
+ */
+public final class ArrivalParetoSetComparatorFactory<T extends McStopArrival<?>> {
+
+  private final ParetoComparator<T> compareRegularStopArrivals;
+  private final ParetoComparator<T> compareStopArrivalsIncludingOnBoardCriteria;
+
+  public ArrivalParetoSetComparatorFactory(ParetoComparator<T> compareArrivalTimeRoundAndC1) {
+    this.compareRegularStopArrivals = compareArrivalTimeRoundAndC1;
+    this.compareStopArrivalsIncludingOnBoardCriteria = compareFunctionWithArrivedOnBoard(
+      compareArrivalTimeRoundAndC1
+    );
+  }
+
+  public static <T extends McStopArrival<?>> ArrivalParetoSetComparatorFactory<T> of(
+    final RelaxFunction relaxC1,
+    @Nullable final DominanceFunction c2DominanceFunction
+  ) {
+    return new ArrivalParetoSetComparatorFactory<>(compareFunction(relaxC1, c2DominanceFunction));
+  }
+
   /**
-   * This comparator is used to compare regular stop arrivals. It uses {@code arrivalTime},
-   * {@code paretoRound} and {@code c1} to compare arrivals. It does NOT include
-   * {@code arrivedOnBoard}. Normally arriving on-board should give the arrival an advantage
-   * - you can continue on foot, walking to the next stop. But, we only do this if it happens
-   * in the same Raptor iteration and round - if it does, it is taken care of by the order
-   * which the algorithm works - not by this comparator.
+   * This comparator is used to compareFunction regular stop arrivals.
+   *  It uses {@code arrivalTime}, {@code paretoRound} and {@code c1} to compareFunction arrivals. It
+   *  does NOT include {@code arrivedOnBoard}. Normally arriving on-board should give the
+   *  arrival an advantage - you can continue on foot, walking to the next stop. But, we only
+   *  do this if it happens in the same Raptor iteration and round - if it does, it is taken
+   *  care of by the order which the algorithm works - not by this comparator.
    */
-  ParetoComparator<T> compareArrivalTimeRoundAndCost();
+  public ParetoComparator<T> compareArrivalTimeRoundAndCost() {
+    return compareRegularStopArrivals;
+  }
 
   /**
    * This includes {@code arrivedOnBoard} in the comparison compared with
    * {@link #compareArrivalTimeRoundAndCost()}.
    */
-  ParetoComparator<T> compareArrivalTimeRoundCostAndOnBoardArrival();
+  public ParetoComparator<T> compareArrivalTimeRoundCostAndOnBoardArrival() {
+    return compareStopArrivalsIncludingOnBoardCriteria;
+  }
 
-  static <T extends McStopArrival<?>> ArrivalParetoSetComparatorFactory<T> of(
+  /* private methods */
+
+  private static <T extends McStopArrival<?>> ParetoComparator<T> compareFunction(
     final RelaxFunction relaxC1,
-    @Nullable final DominanceFunction c2DominanceFunction
+    @Nullable final DominanceFunction c2Function
   ) {
     if (relaxC1.isNormal()) {
-      return c2DominanceFunction == null
-        ? createFactoryC1()
-        : createFactoryC1AndC2(c2DominanceFunction);
+      return c2Function == null
+        ? (l, r) -> compareBase(l, r)
+        : (l, r) -> compareC2(c2Function, l, r);
     }
 
-    return c2DominanceFunction == null
-      ? createFactoryRelaxC1(relaxC1)
-      : createFactoryRelaxC2(relaxC1, c2DominanceFunction);
+    return c2Function == null
+      ? (l, r) -> compareC1Relaxed(relaxC1, l, r)
+      : (l, r) -> compareC1RelaxedOnC2Dominance(relaxC1, c2Function, l, r);
   }
 
-  private static <T extends McStopArrival<?>> ArrivalParetoSetComparatorFactory<
-    T
-  > createFactoryC1() {
-    return new ArrivalParetoSetComparatorFactory<>() {
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundAndCost() {
-        return ArrivalParetoSetComparatorFactory::compareBase;
-      }
-
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundCostAndOnBoardArrival() {
-        return (l, r) -> compareBase(l, r) || compareArrivedOnBoard(l, r);
-      }
-    };
-  }
-
-  private static <T extends McStopArrival<?>> ArrivalParetoSetComparatorFactory<
-    T
-  > createFactoryC1AndC2(DominanceFunction c2DominanceFunction) {
-    return new ArrivalParetoSetComparatorFactory<>() {
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundAndCost() {
-        return (l, r) -> compareBase(l, r) || c2DominanceFunction.leftDominateRight(l.c2(), r.c2());
-      }
-
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundCostAndOnBoardArrival() {
-        return (
-          (l, r) ->
-            compareBase(l, r) ||
-            compareArrivedOnBoard(l, r) ||
-            c2DominanceFunction.leftDominateRight(l.c2(), r.c2())
-        );
-      }
-    };
-  }
-
-  private static <T extends McStopArrival<?>> ArrivalParetoSetComparatorFactory<
-    T
-  > createFactoryRelaxC1(RelaxFunction rc1) {
-    return new ArrivalParetoSetComparatorFactory<>() {
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundAndCost() {
-        return (l, r) -> relaxedCompareBase(rc1, l, r);
-      }
-
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundCostAndOnBoardArrival() {
-        return (l, r) -> relaxedCompareBase(rc1, l, r) || compareArrivedOnBoard(l, r);
-      }
-    };
-  }
-
-  private static <T extends McStopArrival<?>> ArrivalParetoSetComparatorFactory<
-    T
-  > createFactoryRelaxC2(RelaxFunction relaxC1, DominanceFunction c2DominanceFunction) {
-    return new ArrivalParetoSetComparatorFactory<>() {
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundAndCost() {
-        // If c2 dominates, then a slack is added to arrival-time and cost (c1)
-        return (l, r) ->
-          c2DominanceFunction.leftDominateRight(l.c2(), r.c2())
-            ? relaxedCompareBase(relaxC1, l, r)
-            : compareBase(l, r);
-      }
-
-      @Override
-      public ParetoComparator<T> compareArrivalTimeRoundCostAndOnBoardArrival() {
-        return (l, r) ->
-          (c2DominanceFunction.leftDominateRight(l.c2(), r.c2())
-            ? relaxedCompareBase(relaxC1, l, r)
-            : compareBase(l, r)) ||
-          compareArrivedOnBoard(l, r);
-      }
-    };
+  private static <T extends McStopArrival<?>> ParetoComparator<T> compareFunctionWithArrivedOnBoard(
+    ParetoComparator<T> compareArrivalTimeRoundAndC1
+  ) {
+    return (l, r) ->
+      compareArrivalTimeRoundAndC1.leftDominanceExist(l, r) || compareArrivedOnBoard(l, r);
   }
 
   /**
    * Compare arrivalTime, paretoRound and c1.
    */
-  static boolean compareBase(McStopArrival<?> l, McStopArrival<?> r) {
+  private static <T extends McStopArrival<?>> boolean compareBase(T l, T r) {
     // This is important with respect to performance. Using the short-circuit logical OR(||) is
     // faster than bitwise inclusive OR(|) (even between boolean expressions)
     return (
@@ -126,12 +88,28 @@ public interface ArrivalParetoSetComparatorFactory<T extends McStopArrival<?>> {
   }
 
   /**
-   * Compare arrivalTime, paretoRound and c1, relaxing arrivalTime and c1.
+   * Compare arrivalTime, paretoRound, c1, and c21.
    */
-  static boolean relaxedCompareBase(
+  private static <T extends McStopArrival<?>> boolean compareC2(
+    DominanceFunction c2Function,
+    T l,
+    T r
+  ) {
+    return (
+      l.arrivalTime() < r.arrivalTime() ||
+      l.paretoRound() < r.paretoRound() ||
+      l.c1() < r.c1() ||
+      c2Function.leftDominateRight(l.c2(), r.c2())
+    );
+  }
+
+  /**
+   * Compare arrivalTime, paretoRound and c1, relaxing c1.
+   */
+  private static <T extends McStopArrival<?>> boolean compareC1Relaxed(
     final RelaxFunction relaxC1,
-    McStopArrival<?> l,
-    McStopArrival<?> r
+    T l,
+    T r
   ) {
     return (
       l.arrivalTime() < r.arrivalTime() ||
@@ -141,10 +119,24 @@ public interface ArrivalParetoSetComparatorFactory<T extends McStopArrival<?>> {
   }
 
   /**
+   * Compare arrivalTime, paretoRound, and c1 relaxed if c2 dominates.
+   */
+  private static <T extends McStopArrival<?>> boolean compareC1RelaxedOnC2Dominance(
+    final RelaxFunction relaxC1,
+    DominanceFunction c2Function,
+    T l,
+    T r
+  ) {
+    return c2Function.leftDominateRight(l.c2(), r.c2())
+      ? compareC1Relaxed(relaxC1, l, r)
+      : compareBase(l, r);
+  }
+
+  /**
    * Compare arrivedOnBoard. On-board arrival dominate arrive by transfer(foot) since
    * you can continue on foot; hence has more options.
    */
-  static boolean compareArrivedOnBoard(McStopArrival<?> l, McStopArrival<?> r) {
+  private static <T extends McStopArrival<?>> boolean compareArrivedOnBoard(T l, T r) {
     return l.arrivedOnBoard() && !r.arrivedOnBoard();
   }
 }
