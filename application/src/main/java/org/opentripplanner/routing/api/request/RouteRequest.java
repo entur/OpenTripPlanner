05e5eb5d2112f7d04462e7d80552f77dcee0df75
Sigurd Totland <sigurdtotland@gmail.com>
2026-05-28 15:31:13 +0200

    revert: Revert "format: Move !to.isOnBoard validation to RouteRequest.validateOriginAndDestination"
    This reverts commit 07e9f585d99d1837e48df0fe5f8a48f302a6bf94.

--- application/src/main/java/org/opentripplanner/routing/api/request/RouteRequest.java
+++ application/src/main/java/org/opentripplanner/routing/api/request/RouteRequest.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import javax.annotation.Nullable;
-import org.opentripplanner.apis.support.InvalidInputException;
 import org.opentripplanner.model.GenericLocation;
 import org.opentripplanner.model.plan.SortOrder;
 import org.opentripplanner.model.plan.paging.cursor.PageCursor;
@@ -248,12 +247,6 @@ public class RouteRequest implements Serializable {
 
     if (!routingErrors.isEmpty()) {
       throw new RoutingValidationException(routingErrors);
-    }
-
-    if (to.isOnBoard()) {
-      throw new InvalidInputException(
-        "'serviceJourneyLocation' is only supported for the 'from' location, not 'to'"
-      );
     }
   }
 
