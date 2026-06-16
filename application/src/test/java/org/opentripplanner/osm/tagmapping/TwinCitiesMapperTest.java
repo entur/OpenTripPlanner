d16bb49b43162e03ca605831f856291a11632a91
Leonard Ehrenfried <mail@leonard.io>
2026-06-16 13:54:46 +0200

    Improve tests

--- application/src/test/java/org/opentripplanner/osm/tagmapping/TwinCitiesMapperTest.java
+++ application/src/test/java/org/opentripplanner/osm/tagmapping/TwinCitiesMapperTest.java
@@ -3,6 +3,8 @@ package org.opentripplanner.osm.tagmapping;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.opentripplanner.osm.model.OsmWay;
 import org.opentripplanner.osm.wayproperty.WayPropertySet;
 import org.opentripplanner.street.model.StreetTraversalPermission;
@@ -11,12 +13,10 @@ class TwinCitiesMapperTest {
 
   private static final WayPropertySet WPS = new TwinCitiesMapper().buildWayPropertySet();
 
-  @Test
-  void minneapolisSkyway() {
-    var way = OsmWay.of()
-      .withTag("highway", "footway")
-      .withTag("name", "Minneapolis Skyway")
-      .build();
+  @ParameterizedTest
+  @ValueSource(strings = { "Saint Paul Skyway", "Minneapolis Skyway" })
+  void removeSkyway(String name) {
+    var way = OsmWay.of().withTag("highway", "footway").withTag("name", name).build();
 
     assertEquals(StreetTraversalPermission.NONE, WPS.getDataForEntity(way).getPermission());
   }
