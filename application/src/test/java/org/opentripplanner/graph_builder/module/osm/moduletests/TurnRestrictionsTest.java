76b9dafca1ab77d8fe0031c923870d599cae765b
Holger Bruch <hb@mfdz.de>
2026-06-26 13:14:29 +0200

    test: add testcase for issue 2731

--- application/src/test/java/org/opentripplanner/graph_builder/module/osm/moduletests/TurnRestrictionsTest.java
+++ application/src/test/java/org/opentripplanner/graph_builder/module/osm/moduletests/TurnRestrictionsTest.java
@@ -10,11 +10,14 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.opentripplanner.graph_builder.issue.service.DefaultDataImportIssueStore;
 import org.opentripplanner.graph_builder.issues.TurnRestrictionBad;
+import org.opentripplanner.graph_builder.issues.TurnRestrictionUnknown;
 import org.opentripplanner.graph_builder.module.osm.OsmModuleTestFactory;
 import org.opentripplanner.osm.TestOsmProvider;
 import org.opentripplanner.osm.model.OsmNode;
 import org.opentripplanner.osm.model.RelationBuilder;
+import org.opentripplanner.service.osminfo.internal.DefaultOsmInfoGraphBuildRepository;
 import org.opentripplanner.street.geometry.WgsCoordinate;
+import org.opentripplanner.street.graph.Graph;
 
 /**
  * Checks that turn restrictions are processed even if they don't strictly adhere to their
@@ -41,7 +44,12 @@ class TurnRestrictionsTest {
       Arguments.of("no_straight_on", true, node05, node15, node24),
       Arguments.of("no_left_turn", true, node05, node15, node26),
       Arguments.of("no_right_turn", true, node05, node15, node24),
-      Arguments.of("no_u_turn", true, node05, node15, node16)
+      Arguments.of("no_u_turn", true, node05, node15, node16),
+      // Support no_entry & no_exit https://github.com/opentripplanner/OpenTripPlanner/issues/2731
+      Arguments.of("no_entry", false, node05, node15, node16),
+      Arguments.of("no_exit", false, node05, node15, node16),
+      // noexit is a misspelling which we don't check for
+      Arguments.of("noexit", true, node05, node15, node16)
     );
   }
 
@@ -79,7 +87,7 @@ class TurnRestrictionsTest {
       issueStore
         .listIssues()
         .stream()
-        .filter(i -> i instanceof TurnRestrictionBad)
+        .filter(i -> i instanceof TurnRestrictionBad || i instanceof TurnRestrictionUnknown)
         .toList()
     ).hasSize(shouldWarn ? 1 : 0);
   }
