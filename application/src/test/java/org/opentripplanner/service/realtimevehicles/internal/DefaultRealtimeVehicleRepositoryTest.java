657d66857d29eeba6c3b4743da5332d360e1dfed
Vincent Paturet <vincent.paturet@entur.org>
2026-07-16 15:14:31 +0200

    test: use TransitTestEnvironment in realtime-vehicle repository test
    Replace the deprecated TimetableRepositoryForTest fixture with
    TransitTestEnvironment and derive the other-feed and realtime-added
    pattern keys from the scheduled pattern.

--- application/src/test/java/org/opentripplanner/service/realtimevehicles/internal/DefaultRealtimeVehicleRepositoryTest.java
+++ application/src/test/java/org/opentripplanner/service/realtimevehicles/internal/DefaultRealtimeVehicleRepositoryTest.java
@@ -3,8 +3,6 @@ package org.opentripplanner.service.realtimevehicles.internal;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.opentripplanner.street.geometry.WgsCoordinate.GREENWICH;
-import static org.opentripplanner.transit.model._data.TimetableRepositoryForTest.route;
-import static org.opentripplanner.transit.model._data.TimetableRepositoryForTest.tripPattern;
 
 import com.google.common.collect.ImmutableListMultimap;
 import java.time.Instant;
@@ -12,82 +10,80 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.core.model.id.FeedScopedId;
 import org.opentripplanner.service.realtimevehicles.model.RealtimeVehicle;
-import org.opentripplanner.transit.model._data.TimetableRepositoryForTest;
-import org.opentripplanner.transit.model.network.Route;
-import org.opentripplanner.transit.model.network.StopPattern;
+import org.opentripplanner.transit.model.TransitTestEnvironment;
+import org.opentripplanner.transit.model.TripInput;
 import org.opentripplanner.transit.model.network.TripPattern;
+import org.opentripplanner.updater.trip.RealtimeTestConstants;
 
-class DefaultRealtimeVehicleRepositoryTest {
+class DefaultRealtimeVehicleRepositoryTest implements RealtimeTestConstants {
 
-  private static final Route ROUTE = route("r1").build();
-  private static final TimetableRepositoryForTest MODEL = TimetableRepositoryForTest.of();
-  private static final StopPattern STOP_PATTERN = TimetableRepositoryForTest.stopPattern(
-    MODEL.stop("1").build(),
-    MODEL.stop("2").build()
-  );
-  private static final TripPattern PATTERN1 = tripPattern("p1", ROUTE)
-    .withStopPattern(STOP_PATTERN)
-    .build();
-  private static final TripPattern PATTERN2 = tripPattern(
-    "p2",
-    ROUTE.copy().withId(new FeedScopedId("f2", "r2")).build()
-  )
-    .withId(new FeedScopedId("f2", "p2"))
-    .withStopPattern(STOP_PATTERN)
-    .build();
-  private static final Instant TIME = Instant.ofEpochSecond(1000);
   private static final RealtimeVehicle VEHICLE = RealtimeVehicle.builder()
-    .withTime(TIME)
+    .withTime(Instant.ofEpochSecond(1000))
     .withCoordinates(GREENWICH)
     .build();
 
-  private static final String FEED_ID = PATTERN1.getFeedId();
+  private final DefaultRealtimeVehicleRepository repository =
+    new DefaultRealtimeVehicleRepository();
+  private final TripPattern pattern;
+  private final TripPattern patternInOtherFeed;
+  private final String feedId;
+
+  DefaultRealtimeVehicleRepositoryTest() {
+    var envBuilder = TransitTestEnvironment.of();
+    var tripInput = TripInput.of(TRIP_1_ID)
+      .addStop(envBuilder.stop(STOP_A_ID), "12:00:00")
+      .addStop(envBuilder.stop(STOP_B_ID), "12:30:00");
+    var env = envBuilder.addTrip(tripInput).build();
+    pattern = env.tripData(TRIP_1_ID).scheduledTripPattern();
+    patternInOtherFeed = pattern
+      .copy()
+      .withId(new FeedScopedId("f2", "p2"))
+      .withRoute(pattern.getRoute().copy().withId(new FeedScopedId("f2", "r2")).build())
+      .build();
+    feedId = pattern.getFeedId();
+  }
 
   @Test
   void empty() {
-    var repository = new DefaultRealtimeVehicleRepository();
-    assertThat(repository.getRealtimeVehicles(PATTERN1)).isEmpty();
+    assertThat(repository.getRealtimeVehicles(pattern)).isEmpty();
   }
 
   @Test
   void lookupByPattern() {
-    var repository = new DefaultRealtimeVehicleRepository();
-    repository.setRealtimeVehiclesForFeed(FEED_ID, ImmutableListMultimap.of(PATTERN1, VEHICLE));
-    assertEquals(List.of(VEHICLE), repository.getRealtimeVehicles(PATTERN1));
+    repository.setRealtimeVehiclesForFeed(feedId, ImmutableListMultimap.of(pattern, VEHICLE));
+    assertEquals(List.of(VEHICLE), repository.getRealtimeVehicles(pattern));
   }
 
   @Test
   void clearFeed() {
-    var repository = new DefaultRealtimeVehicleRepository();
-    repository.setRealtimeVehiclesForFeed(FEED_ID, ImmutableListMultimap.of(PATTERN1, VEHICLE));
-    repository.setRealtimeVehiclesForFeed(FEED_ID, ImmutableListMultimap.of());
-    assertThat(repository.getRealtimeVehicles(PATTERN1)).isEmpty();
+    repository.setRealtimeVehiclesForFeed(feedId, ImmutableListMultimap.of(pattern, VEHICLE));
+    repository.setRealtimeVehiclesForFeed(feedId, ImmutableListMultimap.of());
+    assertThat(repository.getRealtimeVehicles(pattern)).isEmpty();
   }
 
   @Test
   void keepOtherFeeds() {
-    var repository = new DefaultRealtimeVehicleRepository();
-    repository.setRealtimeVehiclesForFeed(FEED_ID, ImmutableListMultimap.of(PATTERN1, VEHICLE));
+    repository.setRealtimeVehiclesForFeed(feedId, ImmutableListMultimap.of(pattern, VEHICLE));
     repository.setRealtimeVehiclesForFeed(
-      PATTERN2.getFeedId(),
-      ImmutableListMultimap.of(PATTERN2, VEHICLE)
+      patternInOtherFeed.getFeedId(),
+      ImmutableListMultimap.of(patternInOtherFeed, VEHICLE)
     );
-    repository.setRealtimeVehiclesForFeed(FEED_ID, ImmutableListMultimap.of());
-    assertEquals(List.of(VEHICLE), repository.getRealtimeVehicles(PATTERN2));
+    repository.setRealtimeVehiclesForFeed(feedId, ImmutableListMultimap.of());
+    assertEquals(List.of(VEHICLE), repository.getRealtimeVehicles(patternInOtherFeed));
   }
 
   @Test
   void realtimeAddedPattern() {
-    var repository = new DefaultRealtimeVehicleRepository();
-    var realtimePattern = tripPattern("realtime-added", ROUTE)
-      .withStopPattern(STOP_PATTERN)
-      .withOriginalTripPattern(PATTERN1)
+    var realtimePattern = pattern
+      .copy()
+      .withId(new FeedScopedId(feedId, "realtime-added"))
+      .withOriginalTripPattern(pattern)
       .withRealTimeStopPatternModified()
       .build();
     repository.setRealtimeVehiclesForFeed(
-      FEED_ID,
+      feedId,
       ImmutableListMultimap.of(realtimePattern, VEHICLE)
     );
-    assertEquals(List.of(VEHICLE), repository.getRealtimeVehicles(PATTERN1));
+    assertEquals(List.of(VEHICLE), repository.getRealtimeVehicles(pattern));
   }
 }
